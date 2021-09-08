2b828d60aeda3276c25d8a52c50ebaa1d4ea3934
cinekele <cinekele@gmail.com>
2021-09-08 21:12:24 +0200

    feat(repository): add new query to EmployeeRepository

--- src/main/java/pl/envelo/academy/app/company/structure/repository/EmployeeRepository.java
+++ src/main/java/pl/envelo/academy/app/company/structure/repository/EmployeeRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.envelo.academy.app.company.structure.entity.EmployeeEntity;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+    List<EmployeeEntity> getBySupervisor(EmployeeEntity supervisor);
 }
